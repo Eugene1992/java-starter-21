d212106d20273d99e43b8e4adc80b1688a4dee64
Evgeniy <deyneko55@gmail.com>
2017-06-09 21:28:05 +0300

    Added cw examples and hw.

---
+++ src/main/java/cw_02/Variables.java
@@ -0,0 +1,41 @@
+package cw_02;
+
+public class Variables {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+
+        char c1 = 'c';
+        char c2 = 97;
+        char c3 = '\u1234';
+
+        System.out.println(c2);
+        System.out.println(c3);
+
+//        -2^n-1 ... 2^n-1 - 1
+
+        byte b3 = 12;
+
+        short s1 = 1200;
+
+        int i = 10000;
+
+        long l = 100000000000000000L;
+
+        float f1 = 100.0F;
+        float f2 = 100;
+
+        double d1 = 100.0;
+        double d2 = 100;
+
+//        Ctrl + /
+//        double d2 = 100;
+
+//        Ctrl + Shift + /
+        /*System.out.println();
+        System.out.println();*/
+    }
+}
