6ea723943b15c788235a3c8fec141f336dbc1b64
Anton-Revenko <asylum9867112345>
2017-06-11 15:27:42 +0300

    byteShortTask Commit.

---
+++ anton/src/main/java/Hw01/HW3.java
@@ -0,0 +1,14 @@
+package Hw01;
+
+public class HW3 {
+    public static void main(String[] args) {
+        byte b1;
+            b1 = 68;
+        short s1, s2;
+            s1 = 1400;
+            s2 = 2334;
+        System.out.println((s1 + s2) / b1);
+
+
+    }
+}
