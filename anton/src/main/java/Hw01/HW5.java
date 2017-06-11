9c0f355d1d6a342048a5fdbedd4a09c815fe8e90
Anton-Revenko <asylum9867112345>
2017-06-11 19:01:26 +0300

    LastTasks Commit.

---
+++ anton/src/main/java/Hw01/HW5.java
@@ -0,0 +1,20 @@
+package Hw01;
+
+public class HW5 {
+    public static void main(String[] args) {
+        byte b1 = 20;
+        byte b2 = 40;
+        byte b3;
+            b3 = b2;
+            b2 = b1;
+        System.out.println(b3);
+        System.out.println(b2);
+
+        int a;
+        int b = 2;
+            a = b;
+            b = a + b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
