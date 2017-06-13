2c09490b9f451564d60b899d170195b7529fc44d
Resperado <sector-88@mail.ru>
2017-06-13 11:57:44 +0300

    hw1

--- valentin/src/main/java/Test.java
+++ valentin/src/main/java/Test.java
@@ -1,3 +1,5 @@
+
+//hm1
 public class Test {
     public static void main(String[] args) {
 
@@ -8,4 +10,87 @@ public class Test {
     int c =4;
     long v= 9;
 }
+//hm2
+public class Test {
+    public static void main(String[] args) {
+
+        byte s =9 +8;
+        short a= 3-1;
+        int b = 8/2;
+        long f= 120*120;
+        System.out.print(s);
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(s);
+//hw6
+        public class Test {
+            public static void main(String[] args) {
+
+
+            byte b= 8;
+            byte c = 8;
+             byte g = c+b;
+
+                System.out.print(g);
+
+//hw7
+                public class Test {
+                    public static void main(String[] args) {
+
+                    byte sum = 7;
+                        byte num =7;
+                    short a=5;
+                    short b=5;
+
+                    byte sum = a+b/num;
+//hw8
+                        public class Test {
+                            public static void main(String[] args) {
+
+
+                                short b=5;
+                                short c=5 ;
+                                byte f =5 ;
+                                byte d=5 ;
+
+                                short b = (b/c);
+                                 byte f = (f/d);
+
+                              int a = (b*f);
+
+
+System.out.print( a );
+
+                            }
+
+//hw9
+                            public class Test {
+                                public static void main(String[] args) {
+
+
+                                    int a1 =3;
+                                    int a2= 4;
+                                    int a3 =5;
+
+                                     a1=a2;
+                                    a3=a1;
+
+
+                                    System.out.print(a3);
+
+                                    public class Test {
+                                        public static void main(String[] args) {
+
+
+                                            int a = 30;
+                                            int b= 20;
+
+                                            a=15;
+                                            b=16;
+                                        }
+
+
+
+                                }
+
 
