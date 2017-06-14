c60249a602c69e45e33882377487ac021380a913
HodiDim <D_ma@ukr.net>
2017-06-14 19:06:02 +0300

    HW002

---
+++ dima1/src/main/java/HomeWork03/HW02.java
@@ -0,0 +1,26 @@
+package HomeWork03;
+
+import java.util.Scanner;
+
+/**
+ * Created by D_ma on 14.06.2017.
+ */
+public class HW02 {
+    public static void main(String[] args) {
+        System.out.println("Enter the number");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = a / 2;
+        int c = b *2;
+
+        if (a == c) {
+            System.out.println("The number is even. Do not be a ass hole, learn!");
+        }
+        if (a > c) {
+            System.out.println("The number is odd.  Do not be a ass hole, learn!");
+        }
+        if (a == 0) {
+            System.out.println("zero is an integer, since it is divisible by 2.");
+        }
+    }
+}
---
+++ dima1/src/main/java/HomeWork03/HW03.java
@@ -0,0 +1,27 @@
+package HomeWork03;
+
+import java.util.Scanner;
+
+/**
+ * Created by D_ma on 14.06.2017.
+ */
+public class HW03 {
+    public static void main(String[] args) {
+        System.out.println("Enter the number #1");
+        Scanner numberone = new Scanner(System.in);
+        double number1 = numberone.nextInt();
+        System.out.println("Enter the number #2");
+        Scanner numbertwo = new Scanner(System.in);
+        double number2 = numbertwo.nextInt();
+        double ten = 10;
+
+        if (number1 < ten) { /**menshe_10**/
+
+        }
+        if (number1 > ten) { /**bolshe_10**/
+
+        }
+
+
+    }
+}
