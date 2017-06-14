df37b6e6cf5c85b3b02602c08f1ac518e0ca395d
Eugene1992 <deyneko55@gmail.com>
2017-06-14 20:48:54 +0300

    DoWhileTest Commit

---
+++ anton/src/main/java/cwtests/forTest.java
@@ -0,0 +1,15 @@
+package cwtests;
+
+/**
+ * FOR TEST
+ */
+public class forTest {
+    public static void main(String[] args) {
+
+        for (int i = 10; i < 20; i++) {
+
+            System.out.println(i);
+        }
+
+    }
+}
