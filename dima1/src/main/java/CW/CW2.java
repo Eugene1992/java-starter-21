a6a7b3d0e35f70825d9a69008896938185afdbf4
HodiDim <D_ma@ukr.net>
2017-06-14 20:45:29 +0300

    0011

---
+++ dima1/src/main/java/CW/CW2.java
@@ -0,0 +1,16 @@
+package CW;
+
+/**
+ * Created by D_ma on 14.06.2017.
+ */
+public class CW2 {
+    public static void main(String[] args) {
+        int a = 0;
+
+        while (a < 21) {
+            System.out.println(a);
+            a++;
+        }
+
+    }
+}
