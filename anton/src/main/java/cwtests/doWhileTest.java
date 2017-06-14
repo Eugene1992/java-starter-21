9adf73da8ac3c74b4859ad3ebdca1d2860ce5167
Eugene1992 <deyneko55@gmail.com>
2017-06-14 20:44:53 +0300

    DoWhileTest Commit

---
+++ anton/src/main/java/cwtests/doWhileTest.java
@@ -0,0 +1,15 @@
+package cwtests;
+
+/**
+ * DO WHILE TEST
+ */
+public class doWhileTest {
+    public static void main(String[] args) {
+        int i = 10;
+
+        do {
+            System.out.println(i++);
+        } while (i < 20);
+
+    }
+}
