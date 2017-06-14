5f7b5093cb11f35a3d674d6aefe58da4cd383690
Eugene1992 <deyneko55@gmail.com>
2017-06-14 19:18:07 +0300

    Test01 Commit

---
+++ anton/src/main/java/hw02/test01.java
@@ -0,0 +1,21 @@
+package hw02;
+
+import java.util.Scanner;
+
+/**
+ * TEST 01
+ */
+public class test01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String num = scan.next();
+
+        int myNumber = scan.nextInt();
+        if (myNumber % 2 == 0) {
+            System.out.println("Chentoe");
+        } else {
+            System.out.println("Nechetnoe");
+        }
+
+    }
+}
