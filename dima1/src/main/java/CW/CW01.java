e8d7f23cbc75996015057b56046581be5cf7f311
HodiDim <D_ma@ukr.net>
2017-06-14 19:49:45 +0300

    HW002

---
+++ dima1/src/main/java/CW/CW01.java
@@ -0,0 +1,49 @@
+package CW;
+
+import java.util.Scanner;
+
+/**
+ * Created by D_ma on 14.06.2017.
+ */
+public class CW01 {
+    public static void main(String[] args) {
+        System.out.println("Enter the number 1...127");
+        Scanner a = new Scanner(System.in);
+        int i = a.nextInt();
+        int w1 = i / 2;
+        int i1 = i % 2;
+
+        int w2 = i1 / 2;
+        int i2 = i1 % 2;
+
+        int w3 = i2 / 2;
+        int i3 = i2 % 2;
+
+        int w4 = i / 2;
+        int i4 = i % 2;
+
+        int w5 = i / 2;
+        int i5 = i % 2;
+
+        int w1 = i / 2;
+        int i1 = i % 2;
+
+        int w1 = i / 2;
+        int i1 = i % 2;
+
+        int w1 = i / 2;
+        int i1 = i % 2;
+
+        int w1 = i / 2;
+        int i1 = i % 2;
+
+        int w1 = i / 2;
+        int i1 = i % 2;
+
+
+
+
+
+
+    }
+}
