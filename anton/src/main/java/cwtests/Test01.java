df63a5c5cd6e88866b818c19bbda174baf6615d7
Eugene1992 <deyneko55@gmail.com>
2017-06-14 20:53:25 +0300

    Test01 Commit

---
+++ anton/src/main/java/cwtests/Test01.java
@@ -0,0 +1,13 @@
+package cwtests;
+
+/**
+ * TEST 20...30
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        int i = 20;
+        while (i < 30) {
+            System.out.println(i + i++);
+        }
+    }
+}
