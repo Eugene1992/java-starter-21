fc911d3dd725787766c44500889a6061d204bef3
HodiDim <D_ma@ukr.net>
2017-06-14 21:08:49 +0300

    0011

--- dima1/src/main/java/CW/CW2.java
+++ dima1/src/main/java/CW/CW2.java
@@ -8,9 +8,23 @@ public class CW2 {
         int a = 0;
 
         while (a < 21) {
-            System.out.println(a);
+            System.out.print(a + " ");
             a++;
         }
+        System.out.println();
+        int b = 0;
+        for (;b < 21;) {
+            System.out.print(b + " ");
+            b++;
+        }
+        System.out.println();
+        int c = 0;
+        do {
+            System.out.print(c + " ");
+            c++;
+        } while (c < 21);
+
 
     }
+
 }
