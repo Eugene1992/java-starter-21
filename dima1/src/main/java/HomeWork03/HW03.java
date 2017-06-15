8c086955ed5b1d6a641381c2335a16353158dbf8
HodiDim <D_ma@ukr.net>
2017-06-15 20:45:30 +0300

    0011

--- dima1/src/main/java/HomeWork03/HW03.java
+++ dima1/src/main/java/HomeWork03/HW03.java
@@ -2,26 +2,73 @@ package HomeWork03;
 
 import java.util.Scanner;
 
+import static java.lang.System.out;
+
 /**
  * Created by D_ma on 14.06.2017.
  */
 public class HW03 {
     public static void main(String[] args) {
-        System.out.println("Enter the number #1");
+        out.println("Enter the number #1");
         Scanner numberone = new Scanner(System.in);
-        double number1 = numberone.nextInt();
-        System.out.println("Enter the number #2");
+        double a = numberone.nextInt();
+        out.println("Enter the number #2");
         Scanner numbertwo = new Scanner(System.in);
-        double number2 = numbertwo.nextInt();
-        double ten = 10;
-
-        if (number1 < ten) { /**menshe_10**/
+        double b = numbertwo.nextInt();
+        if (a < 10) {  //*Ветка1*//
+            if (b < 10) {
+                double c1 = (10 - a);
+                double c2 = (10 - b);
+                if (c1 < c2) {
+                    System.out.println(a);
+                }
+                if (c1 > c2) {
+                    System.out.println(b);
+                }
+            }
+            if (b > 10) {
+                double c1 = (10 - a);
+                double c2 = (b - 10);
+                if (c1 < c2) {
+                    System.out.println(a);
+                }
+                if (c1 > c2) {
+                    System.out.println(b);
+                }
+            }
 
         }
-        if (number1 > ten) { /**bolshe_10**/
-
+        if (a > 10) {   //*Ветка2*//
+            if (b < 10) {
+                double c1 = (a - 10);
+                double c2 = (10 - b);
+                if (c1 < c2) {
+                    System.out.println(a);
+                }
+                if (c1 > c2) {
+                    System.out.println(b);
+                }
+            }
+            if (b > 10) {
+                double c1 = (a - 10);
+                double c2 = (b - 10);
+                if (c1 < c2) {
+                    System.out.println(a);
+                }
+                if (c1 > c2) {
+                    System.out.println(b);
+                }
+            }
         }
-
-
+        if (a == 10) {
+            System.out.println(10);
+        }
+        if (b == 10) {
+            System.out.println(10);
+        }
+        if (a == b) {
+            System.out.println("I foresaw such a question, and I consider it inappropriate to dismiss it.");
+        }
+      }
     }
-}
+
