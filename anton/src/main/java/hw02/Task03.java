aa94ac617da71b5ae253c6db97d6700ec972d463
Anton-Revenko <asylum9867112345>
2017-06-16 18:14:25 +0300

    SomethingWrongTask03 Commit

---
+++ anton/src/main/java/hw02/Task03.java
@@ -0,0 +1,25 @@
+package hw02;
+
+import java.util.Scanner;
+
+/**
+ * NEAREST TO 10
+ */
+public class Task03 {
+    public static void main(String[] args) {
+        Scanner myNumber = new Scanner(System.in);
+        System.out.println("Enter the first floating number: ");
+        double a = myNumber.nextDouble();
+        System.out.println("Enter the second floating number: ");
+        double b = myNumber.nextDouble();
+        int num = 10;
+        double c = Math.abs(num - a);
+        double d = Math.abs(num - b);
+        if (c > d) {
+            System.out.println(b);
+        } if (d > c) {
+            System.out.println(a);
+        } else {
+            System.out.println("CALL THE PROGRAMMER");
+        }
+    }    }
