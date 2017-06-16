9d51cbd5d700a5a3e052e59716dcf91546e68bc5
Anton-Revenko <asylum9867112345>
2017-06-16 15:19:58 +0300

    IntervalTask Commit

---
+++ anton/src/main/java/hw02/Interval.java
@@ -0,0 +1,30 @@
+package hw02;
+
+import java.util.Scanner;
+
+/**
+ * INTERVAL
+ */
+public class Interval {
+    public static void main(String[] args) {
+        Scanner myNumber = new Scanner(System.in);
+        System.out.println("Enter the number from 0 to 100: ");
+        int num = myNumber.nextInt();
+        if (num < 0 ) {
+            System.out.println("WTF reed the sentence");
+        } else if (num > 0 || num <= 14){
+            System.out.println("First Group: 0 - 14");
+        } else if (num == 15 || num <= 35) {
+            System.out.println("Second Group: 15 - 35");
+        } else if (num == 36 || num <= 50) {
+            System.out.println("Third Group: 36 - 50");
+        } else if (num == 50 || num <= 100){
+            System.out.println("Fourth Group: 50 - 100");
+        } else {
+            System.out.println("WTF reed the sentence");
+        }
+
+
+
+    }
+}
