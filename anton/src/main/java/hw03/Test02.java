1e2fec237841923b576b16e087640d09659af18a
Anton-Revenko <asylum9867112345>
2017-06-17 20:17:24 +0300

    Test02 Commit

---
+++ anton/src/main/java/hw03/Test02.java
@@ -0,0 +1,10 @@
+package hw03;
+public class Test02 {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 55) {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
