38208930dc5dda20643159db033c4a77624e5460
HodiDim <D_ma@ukr.net>
2017-06-17 14:58:20 +0300

    0011

--- dima1/src/main/java/HomeWork03/HW05.java
+++ dima1/src/main/java/HomeWork03/HW05.java
@@ -7,23 +7,85 @@ import java.util.Scanner;
  */
 public class HW05 {
     public static void main(String[] args) {
+        System.out.println("Enter four different numbers.");
         System.out.println("Enter the number#1");
         Scanner numberOne = new Scanner(System.in);
-        int a = numberOne.nextInt();
+        double a = numberOne.nextDouble();
 
         System.out.println("Enter the number#2");
         Scanner numbetTwo = new Scanner(System.in);
-        int b = numbetTwo.nextInt();
+        double b = numbetTwo.nextDouble();
 
         System.out.println("Enter the number#3");
         Scanner numberThree = new Scanner(System.in);
-        int c = numberThree.nextInt();
+        double c = numberThree.nextDouble();
 
         System.out.println("Enter the number#3");
         Scanner numberFore = new Scanner(System.in);
-        int d = numberThree.nextInt();
+        double d = numberThree.nextDouble();
 
-
+        if (a < b) {
+            if (b < c) {
+                if (c < d) {
+                    System.out.println(b);
+                }
+                if (c > d) {
+                    System.out.println(a);
+                }
+                if (c == d) {
+                    System.out.println("Open your eyes. The key word is DIFFERENT!");
+                }
+                if (b > c) {
+                    if (c < d) {
+                        if (d < a) {
+                            System.out.println(d);
+                        }
+                        if (d > a) {
+                            System.out.println(c);
+                        }
+                    }
+                    if (c > d) {
+                        System.out.println(d);
+                    }
+                    if (c == d) {
+                        System.out.println("Open your eyes. The key word is DIFFERENT!");
+                    }
+                }
+            }
+        }
+        if (a > b) {
+            if (b < c) {
+                if (c < d) {
+                    System.out.println(c);
+                }
+                if (c > d) {
+                    System.out.println(b);
+                }
+                if (c == d) {
+                    System.out.println("Open your eyes. The key word is DIFFERENT!");
+                }
+            }
+            if (b > c) {
+                if (c < d) {
+                    if (d < a) {
+                        System.out.println(d);
+                    }
+                    if (d > a) {
+                        System.out.println(c);
+                    }
+                }
+                if (c > d) {
+                    System.out.println(c);
+                }
+                if (c == d) {
+                    System.out.println("Open your eyes. The key word is DIFFERENT!");
+                }
+            }
+        }
+        if (a == b) {
+            System.out.println("Open your eyes. The key word is DIFFERENT!");
+        }
     }
 }
+
 
