f8922b8462cbc3955bcf65b113655c4f498b63bb
HodiDim <D_ma@ukr.net>
2017-06-18 12:36:14 +0300

    0011

---
+++ dima1/src/main/java/HomeWork04/HW03.java
@@ -0,0 +1,16 @@
+package HomeWork04;
+
+/**
+ * Created by D_ma on 18.06.2017.
+ */
+public class HW03 {
+    public static void main(String[] args) {
+        byte a = 90;
+
+        while (a > 0) {
+            System.out.println(a);
+            switch (a = (byte) (a - 5)) {
+            }
+        }
+    }
+}
