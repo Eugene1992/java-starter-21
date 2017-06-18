e2390a88ae42c574843c98d14fe94fd95ae04417
HodiDim <D_ma@ukr.net>
2017-06-18 14:22:37 +0300

    0011

--- dima1/src/main/java/HomeWork04/HW03.java
+++ dima1/src/main/java/HomeWork04/HW03.java
@@ -7,7 +7,7 @@ public class HW03 {
     public static void main(String[] args) {
         byte a = 90;
 
-        while (a > 0) {
+        while (a >= 0) {
             System.out.println(a);
             switch (a = (byte) (a - 5)) {
             }
