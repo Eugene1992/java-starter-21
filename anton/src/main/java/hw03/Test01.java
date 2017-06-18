17f00394eb32e7e4ec4a2e46cca2520f98530c90
Anton-Revenko <asylum9867112345>
2017-06-18 14:16:40 +0300

    Test01 Commit

---
+++ anton/src/main/java/hw03/Test01.java
@@ -0,0 +1,8 @@
+package hw03;
+public class Test01 {
+    public static void main(String[] args) {
+        for (int i = 0, j = 1000; i < 6; i++, j += 3) {
+            System.out.println(j);
+        }
+    }
+}
