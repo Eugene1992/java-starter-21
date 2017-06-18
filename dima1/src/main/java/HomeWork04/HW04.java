64b877714f93c6f3f752626a1c3cff42a9a92143
HodiDim <D_ma@ukr.net>
2017-06-18 13:13:10 +0300

    0011

---
+++ dima1/src/main/java/HomeWork04/HW04.java
@@ -0,0 +1,17 @@
+package HomeWork04;
+
+/**
+ * Created by D_ma on 18.06.2017.
+ */
+public class HW04 {
+    public static void main(String[] args) {
+
+
+        for (int a = 0, b = 1; a < 20; b = b * 2 ) {
+            System.out.println(b);
+            a++;
+        }
+        System.out.println("End");
+        }
+    }
+
