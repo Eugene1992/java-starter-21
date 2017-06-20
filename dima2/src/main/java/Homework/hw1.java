2febad06720133a748ffb35ae52edf1122405dd7
Дима <Denar@ukr.net>
2017-06-20 23:30:30 +0300

    1

--- dima2/src/main/java/Homework/hw1.java
+++ dima2/src/main/java/Homework/hw1.java
@@ -1,7 +1,41 @@
 package Homework;
 
+import java.util.Scanner;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
 /**
  * Created by Дима on 20.06.2017.
  */
 public class hw1 {
+    public static class Calculator {
+        public static void main(String[] args) {
+            Scanner a = new Scanner(in);
+            int First, Second;
+            out.println("Введите первое число");
+            First = a.nextInt();
+            out.println("Введите второе число число");
+            Second = a.nextInt();
+            out.println("Че надо с ними сделать?=)");
+            String operation = a.next();
+            switch (operation) {
+                case "+":
+                    out.println(First + Second);
+                    break;
+                case "-":
+                    out.println(First - Second);
+                    break;
+                case "*":
+                    out.println(First * Second);
+                    break;
+                case "/":
+                    out.println(First / Second);
+                    break;
+
+
+            }
+        }
+
+    }
 }
