01b5c2fba35c5e281bb69de0fe2c0cc656aff3c8
Дима <Denar@ukr.net>
2017-06-20 20:53:09 +0300

    Четко

--- dima2/src/main/java/Test.java
+++ dima2/src/main/java/Test.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
 public class Test {
     public static void main(String[] args) {
-     byte a = 3;
-     byte b = 5;
-     int c = (byte) (a + b);
-        System.out.println(c);
+        Scanner a = new Scanner(in);
+        int First, Second;
+        int Do;
+        out.println("Введите первое число");
+        First = (int) a.nextFloat();
+        out.println("Введите второе число число");
+        Second = (int) a.nextFloat();
+        out.println("Че надо с ними сделать?=)");
+        String operation = a.next();
+        switch (operation) {
+            case "+":
+                out.println(First + Second);
+                break;
+            case "-":
+                out.println(First - Second);
+                break;
+            case "*":
+                out.println(First * Second);
+                break;
+            case "/":
+                out.println(First / Second);
+                break;
 
+
+        }
     }
+
 }
