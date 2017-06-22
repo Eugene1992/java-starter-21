2a49a295f05011c2e04f4e11a6bdab1fcf8ad15a
HodiDim <D_ma@ukr.net>
2017-06-22 22:07:42 +0300

    001112

--- dima1/src/main/java/homework07/HW01.java
+++ dima1/src/main/java/homework07/HW01.java
@@ -3,17 +3,19 @@ package homework07;
 /**
  * Created by D_ma on 22.06.2017.
  */
-public class HW {
+public class HW01 {
     public static void main(String[] args) {
-        double [] myArey = {0, 1, 2, 3, 4 , 5, 6, 7, 8, 9};
+        double[] myArey = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         cl(myArey);
+
     }
-    static  void cl (double [] myArey) {
+
+    static void cl(double[] myArey) {
         for (int i = 0; i < myArey.length; i++) {
             System.out.println(myArey[i]);
 
         }
-
-
     }
 }
+
+
---
+++ dima1/src/main/java/homework07/HW02.java
@@ -0,0 +1,18 @@
+package homework07;
+
+/**
+ * Created by D_ma on 22.06.2017.
+ */
+public class HW02 {
+    public static void main(String[] args) {
+        double [] iddqd = {10, 11, 12, 13, 14, 15, 16, 17, 18};
+
+        pum(iddqd);
+    }
+    static void pum (double [] iddqd) {
+        for (int i = 8; i >= 0; i--) {
+            System.out.println(iddqd [i]);
+
+        }
+    }
+}
